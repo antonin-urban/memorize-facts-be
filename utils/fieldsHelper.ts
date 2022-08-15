2199bb3b3f0960206bcf85cd954799212a2e57c4
Antonin Urban <urban.antonin@gmail.com>
2022-08-15 19:48:17 +0200

    draft: add new field types

--- utils/fieldsHelper.ts
+++ utils/fieldsHelper.ts
@@ -1,8 +1,9 @@
-import { text } from '@keystone-6/core/fields';
+import { text, checkbox, timestamp } from '@keystone-6/core/fields';
 
 export const ownerId = text({
   access: {
     update: () => false,
+    create: () => false,
   },
   ui: {
     createView: {
@@ -11,8 +12,53 @@ export const ownerId = text({
     itemView: {
       fieldMode: 'read',
     },
+    listView: {
+      fieldMode: 'read',
+    },
   },
   graphql: {
     omit: true,
+  },
+});
+
+export const deleted = checkbox({
+  access: {
+    update: () => false,
+    create: () => false,
+  },
+  ui: {
+    createView: {
+      fieldMode: 'hidden',
+    },
+    itemView: {
+      fieldMode: 'hidden',
+    },
+    listView: {
+      fieldMode: 'hidden',
+    },
+  },
+  graphql: {
+    omit: true,
+  },
+});
+
+export const updatedAt = timestamp({
+  access: {
+    update: () => false,
+    create: () => false,
+  },
+  ui: {
+    createView: {
+      fieldMode: 'hidden',
+    },
+    itemView: {
+      fieldMode: 'read',
+    },
+    listView: {
+      fieldMode: 'read',
+    },
+  },
+  db: {
+    updatedAt: true,
   },
 });
